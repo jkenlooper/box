0daddf916a66d4bf77d9ee1e2ef4384629606d31
Jake Hickenlooper <jake@weboftomorrow.com>
2017-11-05 10:32:10 -0700

    Update init script

--- init.sh
+++ init.sh
@@ -9,9 +9,11 @@ usermod -aG sudo $INIT_USER
 read -n 1 -p "Copy the id_rsa.pub key for the remote ... press any key to continue";
 
 mkdir -p /home/$INIT_USER/.ssh
+chown jake:jake /home/$INIT_USER/.ssh
 chmod 700 /home/$INIT_USER/.ssh
 read -p "paste in the key to append to .ssh/authorized_keys: " PUB_KEY
-echo $PUB_KEY >> /home/$INIT_USER/.ssh/authorized_keys
+echo "$PUB_KEY" >> /home/$INIT_USER/.ssh/authorized_keys
+chown jake:jake /home/$INIT_USER/.ssh/authorized_keys
 chmod 600 /home/$INIT_USER/.ssh/authorized_keys
 
 echo "Enable firewall"
