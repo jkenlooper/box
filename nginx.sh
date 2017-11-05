db2ab0b1380215507f8afec14fd21f0c3af79d1e
Jake Hickenlooper <jake@weboftomorrow.com>
2017-11-05 10:31:54 -0700

    Update nginx initial setup

--- nginx.sh
+++ nginx.sh
@@ -8,6 +8,10 @@ sudo apt-get --yes install apache2-utils
 echo "Create the .htpasswd file"
 htpasswd -c $HOME/.htpasswd jake
 
+echo "Creating error pages in $HOME/public/"
+mkdir -p $HOME/public
+cp nginx/*.html $HOME/public/
+
 read -p "Update ip in nginx/local.conf"
 sudo cp nginx/local.conf /etc/nginx/sites-available/local.conf
 sudo ln -fs /etc/nginx/sites-available/local.conf /etc/nginx/sites-enabled/local.conf
